02a870ec41c05d3ae5c4ccb45d8f5cc831ff11e4
Dionna Glaze <dionnaglaze@google.com>
2018-03-12 11:08:24 -0700

    Build mods to link boringssl

--- buildenv.mk
+++ buildenv.mk
@@ -99,7 +99,8 @@ COMMON_FLAGS += -ffunction-sections -fdata-sections
 COMMON_FLAGS += -Wall -Wextra -Winit-self -Wpointer-arith -Wreturn-type \
 		-Waddress -Wsequence-point -Wformat-security \
 		-Wmissing-include-dirs -Wfloat-equal -Wundef -Wshadow \
-		-Wcast-align -Wconversion -Wredundant-decls
+		-Wcast-align -Wconversion -Wredundant-decls \
+		-Wno-unused-variable  # Protobuf produces unused variables
 
 # additional warnings flags for C
 CFLAGS += -Wjump-misses-init -Wstrict-prototypes -Wunsuffixed-float-constants
